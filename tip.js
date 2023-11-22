10a588e2a30fbd0d430f9942469897e6e4e7b7e6
itz4lex <148805735+itz4lex@users.noreply.github.com>
2023-11-21 21:01:48 -0600

    Update tip.js

--- tip.js
+++ tip.js
@@ -3,25 +3,25 @@ function twenty() {
   
     var percent = document.getElementById("userInput").value; 
       
-    var num = document.getElementById("userInput").value; 
+    var num = document.getElementById("userOutput").value; 
     document.getElementById("tip20") 
-        .value = (num / 100) * percent; 
+        .value = (userInput / 100) * percent; 
 } 
   
 function five() { 
   
     var num1 = document.getElementById("userInput").value; 
        
-    var num2 = document.getElementById("userInput").value; 
+    var num2 = document.getElementById("userOutput").value; 
     document.getElementById("tip25") 
-            .value = (num1 * 100) / num2 + "%"; 
+            .value = (userInput * 100) / "tip25"; 
 }
 function fifteen() { 
   
    
     var num1 = document.getElementById("userInput").value; 
       
-    var num2 = document.getElementById("userInput").value; 
+    var num2 = document.getElementById("userOutput").value; 
     document.getElementById("tip15") 
-            .value = (num1 * 100) / num2 + "%"; 
+            .value = (userInput * 100) / "tip15"; 
 }
