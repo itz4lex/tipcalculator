1b6b469f05f15ee3dccf5558d7b67e6e3edbf4c8
itz4lex <148805735+itz4lex@users.noreply.github.com>
2023-11-21 20:08:34 -0600

    Add files via upload

---
+++ tip.js
@@ -0,0 +1,11 @@
+function twenty() {   
+    return (num/100)*per;  
+    }   
+
+function five() {   
+    return (num/100)*per;  
+        }   
+    
+function fifteen() {   
+    return (num/100)*per;  
+            }   
